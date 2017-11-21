ebb191e9fedd17f6cd554c87ea233e2879319d9f
21646436 <21646436@C306-26.alumnos.uem.es>
2017-11-21 20:59:23 +0100

    control + alt + f

--- splashscreen/src/sample/Controller.java
+++ splashscreen/src/sample/Controller.java
@@ -33,10 +33,9 @@ public class Controller implements Initializable {
 
 	@FXML
 	private Label nameLabel;
-	
+
 	@FXML
 	private ImageView mountains;
-	
 
 	Main main;
 	Stage stage;
@@ -54,9 +53,9 @@ public class Controller implements Initializable {
 			AnchorPane page = (AnchorPane) loader.load();
 			Stage sendStage = new Stage();
 			sendStage.setTitle("Main Menu");
-			Scene scene=new Scene(page);
+			Scene scene = new Scene(page);
 			sendStage.setScene(scene);
-//			sendStage.setMaximized(true);
+			// sendStage.setMaximized(true);
 			sendStage.show();
 			System.out.println("second stage ok");
 
@@ -67,79 +66,69 @@ public class Controller implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-		TranslateTransition translateTransition = new TranslateTransition(
-				Duration.seconds(0.5), logoLabel);
+
+		TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), logoLabel);
 		translateTransition.setByY(700);
 		translateTransition.play();
 
-		TranslateTransition translateTransition0 = new TranslateTransition(
-				Duration.seconds(0.5), nameLabel);
+		TranslateTransition translateTransition0 = new TranslateTransition(Duration.seconds(0.5), nameLabel);
 		translateTransition0.setByY(700);
 		translateTransition0.play();
 
-		TranslateTransition translateTransition00 = new TranslateTransition(
-				Duration.seconds(0.5), helloLabel);
+		TranslateTransition translateTransition00 = new TranslateTransition(Duration.seconds(0.5), helloLabel);
 		translateTransition00.setByY(700);
 		translateTransition00.play();
 
-		translateTransition
-				.setOnFinished(event -> {
-					TranslateTransition translateTransition1 = new TranslateTransition(
-							Duration.seconds(1), logoLabel);
-					translateTransition1.setByY(-700);
-					translateTransition1.play();
+		translateTransition.setOnFinished(event -> {
+			TranslateTransition translateTransition1 = new TranslateTransition(Duration.seconds(1), logoLabel);
+			translateTransition1.setByY(-700);
+			translateTransition1.play();
 
-					translateTransition1.setOnFinished(event1 -> {
+			translateTransition1.setOnFinished(event1 -> {
 
-						nameLabel.setVisible(true);
+				nameLabel.setVisible(true);
 
-						TranslateTransition translateTransition11 = new TranslateTransition(
-								Duration.seconds(1), nameLabel);
-						translateTransition11.setByY(-700);
-						translateTransition11.play();
+				TranslateTransition translateTransition11 = new TranslateTransition(Duration.seconds(1), nameLabel);
+				translateTransition11.setByY(-700);
+				translateTransition11.play();
 
-						translateTransition11.setOnFinished(event2 -> {
+				translateTransition11.setOnFinished(event2 -> {
 
-							helloLabel.setVisible(true);
-							TranslateTransition translateTransition111 = new TranslateTransition(
-									Duration.seconds(1), helloLabel);
-							translateTransition111.setByY(-700);
-							translateTransition111.play();
+					helloLabel.setVisible(true);
+					TranslateTransition translateTransition111 = new TranslateTransition(Duration.seconds(1),
+							helloLabel);
+					translateTransition111.setByY(-700);
+					translateTransition111.play();
 
-							translateTransition111.setOnFinished(event3 -> {
-								spinnerPane.setVisible(true);
+					translateTransition111.setOnFinished(event3 -> {
+						spinnerPane.setVisible(true);
 
-								FadeTransition fadeTransition = new FadeTransition(
-										Duration.seconds(1), spinnerPane);
-								fadeTransition.setFromValue(0);
-								fadeTransition.setToValue(1);
-								fadeTransition.play();
+						FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), spinnerPane);
+						fadeTransition.setFromValue(0);
+						fadeTransition.setToValue(1);
+						fadeTransition.play();
 
-								fadeTransition.setOnFinished(event4 -> {
+						fadeTransition.setOnFinished(event4 -> {
 
-									FadeTransition fadeTransition1 = new FadeTransition(
-											Duration.seconds(1), rootPane);
-									fadeTransition1.setFromValue(1);
-									fadeTransition1.setToValue(0.1);
-									fadeTransition1.play();
+							FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(1), rootPane);
+							fadeTransition1.setFromValue(1);
+							fadeTransition1.setToValue(0.1);
+							fadeTransition1.play();
 
-									fadeTransition1.setOnFinished(event5 -> {
-										main.closeStage();
-										showMenu();
-										System.out
-												.println("------- splash screen is closed --------");
-									});
-
-								});
-
+							fadeTransition1.setOnFinished(event5 -> {
+								main.closeStage();
+								showMenu();
+								System.out.println("------- splash screen is closed --------");
 							});
 
 						});
+
 					});
 
 				});
+			});
+
+		});
 
 	}
 }
